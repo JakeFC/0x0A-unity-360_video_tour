9bd5077811447235366eed9a5b3a73b5e763d4f5
JakeFC <jacobchavera@yahoo.com>
2021-12-01 06:02:47 -0800

    Task 0

---
+++ Assets/Scripts/FlipNormals.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using UnityEngine;
+
+public class FlipNormals : MonoBehaviour
+{
+    void Start()
+    {
+        Mesh mesh = this.GetComponent<MeshFilter>().mesh;
+
+        Vector3[] normals = mesh.normals;
+        for (int i = 0; i < normals.Length; i++)
+        {
+            // flip normals
+            normals[i] = -normals[i];
+        }
+
+        mesh.normals = normals;
+
+        for (int i = 0; i < mesh.subMeshCount; i++)
+        {
+            int[] tris = mesh.GetTriangles(i);
+            for (int j = 0; j < tris.Length; j += 3)
+            {
+                // swap polygon vertices
+                int temp = tris[j];
+                tris[j] = tris[j + 1];
+                tris[j + 1] = temp;
+            }
+            mesh.SetTriangles(tris, i);
+        }
+    }
+}
