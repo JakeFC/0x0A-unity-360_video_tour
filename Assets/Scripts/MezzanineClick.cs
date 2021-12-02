c4b3b4f30d198461600fdffdd3cd680b60b57b1a
JakeFC <jacobchavera@yahoo.com>
2021-12-01 19:36:01 -0800

    Task 3

--- Assets/Scripts/MezzanineClick.cs
+++ Assets/Scripts/MezzanineClick.cs
@@ -6,13 +6,45 @@ public class MezzanineClick : MonoBehaviour
 {
     public GameObject mezzanine;
     public GameObject player;
-    // Moves the camera to the chosen video area after a transition and
-    // enables the relevant game object, while disabling the current one
-    public void Transport()
+    public SpriteRenderer blackScreen;
+
+    public void Transition()
     {
         mezzanine.SetActive(true);
+        StartCoroutine("Fade");
+    }
+    // Moves the camera to the chosen video area after a transition and
+    // enables the relevant game object, while disabling the current one
+    private void Transport()
+    {
         player.transform.position = new Vector3(10f, 0f, 10f);
+    }
+
+    public IEnumerator Fade()
+    {
+        Color imageColor = blackScreen.color;
+        float fadeAmount;
+
+        while (imageColor.a < 1)
+        {
+            fadeAmount = imageColor.a + (Time.deltaTime);
+
+            imageColor = new Color(imageColor.r, imageColor.g, imageColor.b, fadeAmount);
+            blackScreen.color = imageColor;
+            yield return null;
+        }
+
+        Transport();
+
+        while (imageColor.a > 0)
+        {
+            fadeAmount = imageColor.a - (Time.deltaTime);
+
+            imageColor = new Color(imageColor.r, imageColor.g, imageColor.b, fadeAmount);
+            blackScreen.color = imageColor;
+            yield return null;
+        }
+
         transform.parent.gameObject.SetActive(false);
-        gameObject.GetComponent<ButtonHover>().UnHover();
     }
 }
